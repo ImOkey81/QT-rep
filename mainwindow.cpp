bf3268283e715150356180aa960e8b0ced18ffa8
ImOkey81 <artem.to2018@icloud.com>
2025-11-24 22:48:52 +0300

    Add Hello World UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,27 @@
 #include "mainwindow.h"
+
 #include <QLabel>
+#include <QFont>
+#include <QVBoxLayout>
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QLabel *label = new QLabel("Hello World!", this);
+    auto *central = new QWidget(this);
+    auto *layout = new QVBoxLayout(central);
+
+    auto *label = new QLabel("Hello, World!", central);
+    QFont labelFont = label->font();
+    labelFont.setPointSize(18);
+    labelFont.setBold(true);
+    label->setFont(labelFont);
     label->setAlignment(Qt::AlignCenter);
-    setCentralWidget(label);
+
+    layout->addWidget(label);
+    layout->setContentsMargins(24, 24, 24, 24);
+
+    setCentralWidget(central);
     setWindowTitle("Hello World App");
     resize(400, 300);
 }
